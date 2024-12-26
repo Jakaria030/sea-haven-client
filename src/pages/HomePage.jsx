374990af81dd6e1114568133e447150ce4801fd9
Jakaria030 <gulamjakaria99@gmail.com>
2024-12-26 13:30:48 +0600

    Our service section added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,11 +7,15 @@ import Testimonial from '../components/Testimonial';
 import offerImage from '../assets/offer.png';
 import Title from '../components/Title';
 import { HiUserGroup } from 'react-icons/hi';
-import { FaCommentsDollar, FaHotel, FaStar } from 'react-icons/fa';
+import { FaCommentsDollar, FaHotel, FaStar, FaWifi } from 'react-icons/fa';
 import CountUp from 'react-countup';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useInView } from 'react-intersection-observer';
+import ourHotel from '../assets/hotel.png';
+import { MdLocalLaundryService } from 'react-icons/md';
+import { IoIosBed, IoIosTv } from 'react-icons/io';
+import NoDataFound from '../components/NoDataFound';
 
 
 const HomePage = () => {
@@ -56,8 +60,8 @@ const HomePage = () => {
 
     const [countUp, setCountUp] = useState(null);
     useEffect(() => {
-        const fetchForCountUp = async() =>{
-            const {data} = await axios.get(`${baseURL}/count-up`);
+        const fetchForCountUp = async () => {
+            const { data } = await axios.get(`${baseURL}/count-up`);
             setCountUp(data);
         };
 
@@ -101,14 +105,14 @@ const HomePage = () => {
             {/* featured room */}
             <section className='max-w-8xl mx-auto px-5'>
                 <h2 className='text-3xl font-semibold text-center text-secondary my-8'>Featured Rooms</h2>
-                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+                {rooms ? (<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
                     {
                         rooms.map(room => <TopRoomCard
                             key={room._id}
                             room={room}
                         ></TopRoomCard>)
                     }
-                </div>
+                </div>) : <NoDataFound></NoDataFound>}
             </section>
 
             {/* count up section */}
@@ -153,6 +157,61 @@ const HomePage = () => {
                 <h2 className='text-3xl pt-10 font-semibold text-center text-secondary my-8'>User Reviews</h2>
                 <div className='max-w-8xl mx-auto px-5'>
                     <Testimonial></Testimonial>
+                </div>
+            </section>
+
+            {/* our services */}
+            <section className='max-w-8xl mx-auto px-5 pb-10'>
+                <div className='flex flex-col sm:flex-row gap-5'>
+                    <figure className='basis-1/2 rounded-md flex items-center justify-center'>
+                        <img className='rounded-xl p-2 bg-white' src={ourHotel} />
+                    </figure>
+                    <div className='basis-1/2 space-y-5'>
+                        <h2 className='text-3xl font-semibold text-secondary '>Our Services</h2>
+
+                        <div className='space-y-3'>
+                            <div className="collapse collapse-plus border-2 border-white">
+                                <input type="radio" name="my-accordion-3" />
+                                <div className="collapse-title text-xl font-medium flex items-center gap-2">
+                                    <FaWifi className='text-3xl' />
+                                    <h3>Wi-Fi Internet</h3>
+                                </div>
+                                <div className="collapse-content">
+                                    <p>Enjoy seamless high-speed WiFi connectivity throughout your stay, keeping you connected at all times.</p>
+                                </div>
+                            </div>
+                            <div className="collapse collapse-plus border-2 border-white">
+                                <input type="radio" name="my-accordion-3" />
+                                <div className="collapse-title text-xl font-medium flex items-center gap-2">
+                                    <IoIosTv  className='text-3xl' />
+                                    <h3>TV in Bedroom</h3>
+                                </div>
+                                <div className="collapse-content">
+                                    <p>Relax and unwind with a flat-screen TV in every bedroom, offering entertainment at your fingertips.</p>
+                                </div>
+                            </div>
+                            <div className="collapse collapse-plus border-2 border-white">
+                                <input type="radio" name="my-accordion-3" />
+                                <div className="collapse-title text-xl font-medium flex items-center gap-2">
+                                    <MdLocalLaundryService className='text-3xl' />
+                                    <h3>Laundry Services</h3>
+                                </div>
+                                <div className="collapse-content">
+                                    <p>Experience hassle-free living with our convenient and reliable laundry services, tailored to your needs.</p>
+                                </div>
+                            </div>
+                            <div className="collapse collapse-plus border-2 border-white">
+                                <input type="radio" name="my-accordion-3" />
+                                <div className="collapse-title text-xl font-medium flex items-center gap-2">
+                                    <IoIosBed className='text-3xl' />
+                                    <h3>Breakfast in Bed</h3>
+                                </div>
+                                <div className="collapse-content">
+                                    <p>Indulge in the luxury of breakfast in bed, delivered fresh to start your day with comfort and style.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
 
